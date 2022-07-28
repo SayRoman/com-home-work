216efda038cfc25a1b63e8b5ff9d5ee425906f7e
Lebedevanata19021988 <reklama.bapb@yandex.ru>
2022-07-28 18:30:50 +0300

    added new giles

--- HW/src/Pr1/Spaceship.java
+++ HW/src/Pr1/Spaceship.java
@@ -1,22 +1,21 @@
-package Pr1;
-
-import java.util.Scanner;
-
 public class Spaceship {
+
+
+
     public static void main(String[] args) {
-        name();
-        age();
-    }
-    static void name() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Введите ваше имя: ");
-        String fisrtName = s.next();
-        System.out.print("Ваше имя: " + fisrtName + "!\n");
-    }
-    static void age (){
-        Scanner s = new Scanner(System.in);
-        System.out.print("Введите ваш возраст: ");
-        int age = s.nextInt();
-        System.out.print("Ваш возраст: " + age);
+
+        Spaceships obj = new Spaceships();
+        Spaceships obj1 = new Spaceships();
+
+        obj.name = "Valinor";
+        obj1.name = "Shir";
+        obj.speed = 1232;
+        obj1.speed = 1000;
+
+        obj.showSpaceShipName();
+        obj.showSpaceShipSpeed();
+        obj1.showSpaceShipName();
+        obj1.showSpaceShipSpeed();
+
     }
 }
---
+++ HW/src/Pr1/Spaceships.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Spaceships {
+
+    public String name;
+
+    public int speed;
+
+    public void showSpaceShipName() {
+        System.out.println(name);
+    }
+    public void showSpaceShipSpeed() {
+        System.out.println(speed);
+    }
+}
